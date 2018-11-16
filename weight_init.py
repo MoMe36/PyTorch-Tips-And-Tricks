251eef3bd6ccd1db2b9af4cebe37736c8bc0d284
Mehd6384 <mounsif.mehdi@gmail.com>
2018-11-16 09:09:38 +0100

    Added xavier init

--- weight_init.py
+++ weight_init.py
@@ -11,12 +11,18 @@ def weight_init(m):
 		m.weight.data.normal_(0.,0.02)
 		m.bias.data.zero_()
 
+def xavier_init(m): 
+
+	if isinstance(m, nn.Linear): 
+		nn.init.xavier_normal_(m.weight)
+		m.bias.data.zero_()
+
 for l in model: 
 	print(l)
 	for p in l.parameters():
 		print(p)
 
-model.apply(weight_init)
+model.apply(xavier_init)
 
 for l in model: 
 	print(l)
